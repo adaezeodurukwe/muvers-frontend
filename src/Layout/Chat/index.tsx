0ef896df0e4bebc8eefcb1084dfb7cde09a0328a
adaezeodurukwe <adaeze.od@softcom.ng>
2020-09-18 06:12:49 +0100

    start user messaging

--- src/Layout/Chat/index.tsx
+++ src/Layout/Chat/index.tsx
@@ -1,21 +1,57 @@
 import React, { useEffect, useState, FormEvent, ChangeEvent } from "react";
 import io from "socket.io-client";
-import SendIcon from '@material-ui/icons/Send';
+import SendIcon from "@material-ui/icons/Send";
 import { TextField, InputAdornment, IconButton } from "@material-ui/core";
-import "./index.scss"
+import "./index.scss";
 
 const url = "http://localhost:8000";
 const socket = io(url);
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZmlyc3ROYW1lIjoiQWRhZXplIiwibGFzdE5hbWUiOiJzb29uIiwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsInBob25lIjoiMDkwOTg4OTc2NSIsImFjY291bnRUeXBlIjoiY2xpZW50IiwiY3JlYXRlZEF0IjoiMjAyMC0wOS0xMVQwNTo0NzoyMC4wMzFaIiwidXBkYXRlZEF0IjoiMjAyMC0wOS0xMVQwNTo0NzoyMC4wMzFaIiwiaWF0IjoxNTk5ODAzMjUyfQ.YMZUGtv-1bc8VX-g_hfpzFsg3V9ttT4gZTN-0uMZ3Qc";
 
+interface ReturnData {
+  newChat: Chat,
+  connection: Connection
+};
+
+interface Chat {
+
+}
+
+interface Connection {
+  id: number,
+  chat: Chat[]
+}
+
 const Chat = () => {
+  const [connectionId, setConnectionId] = useState(0);
   const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]) as any;
   useEffect(() => {
     socket.emit("authenticate", { token });
   }, []);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log(chat, connectionId);
+  }, [chat, connectionId])
+
+  useEffect(() => {
+    socket.on("success", (data: ReturnData) => {
+      console.log(data);
+      setConnectionId(data.connection.id)
+    });
+  }, [setChat,setConnectionId]);
+
+  useEffect(() => {
+    socket.on("conversation", (data: ReturnData) => {
+      if (data.connection) {
+        setChat(data.connection.chat)
+      }
+      if (data.newChat) {
+        setChat([...chat, data.newChat])
+      }      
+    })
+  }, [chat, setChat])
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -24,6 +60,14 @@ const Chat = () => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(message,  `${connectionId}-message`);
+    socket.emit(`${connectionId}-message`, {
+      userId: 2,
+      parentId: 1,
+      senderName: "Adaeze",
+      connectionId,
+      message,
+    });
   };
 
   return (
